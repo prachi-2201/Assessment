ef8c366276a7cbb362c0f1342caf0c82302a0071
Prachi <prachipatel2201@gmail.com>
2024-01-28 11:05:35 +0530

    data

--- Assesment_3/script.js
+++ Assesment_3/script.js
@@ -1,11 +1,11 @@
 var menu=document.getElementById("menu")
+// function on click
 function dropdown()
 {
     console.log("he")
 
+    // adding class to menu for changing display 
     menu.classList.add("dropdown")
-    
-
 
 };
 
